c796bc15bc49af226f98c3e42384a319519cf165
Oleh Didushok <didushokov@gmail.com>
2025-09-24 21:52:27 +0300

    Configured increment and decrement

--- Todo/Todo/ContentView.swift
+++ Todo/Todo/ContentView.swift
@@ -6,19 +6,86 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+struct CounterFeature: Reducer {
+    func reduce(into state: inout State, action: Action) -> ComposableArchitecture.Effect<Action> {
+        switch action {
+            
+        case .decrementButtonTapped:
+            state.count -= 1
+            return .none
+            
+        case .incrementButtonTapped:
+            state.count += 1
+            return .none
+            
+        case .getFactButtonTapped:
+            return .none
+            
+        case .toggleTimerButtonTapped:
+            state.isTimerOn.toggle()
+            return .none
+        }
+    }
+    
+    @ObservableState
+    struct State: Equatable, Sendable {
+        var count = 0
+        var fact: String?
+        var isTimerOn = false
+    }
+    
+    enum Action: Sendable {
+        case decrementButtonTapped
+        case getFactButtonTapped
+        case incrementButtonTapped
+        case toggleTimerButtonTapped
+    }
+}
 
 struct ContentView: View {
+    let store: StoreOf<CounterFeature>
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section {
+                Text("\(store.count)")
+                Button("Decrement") {
+                    store.send(.decrementButtonTapped)
+                }
+                Button("Increment") {
+                    store.send(.incrementButtonTapped)
+                }
+            }
+            Section {
+                Button("Get fact") {
+                    store.send(.getFactButtonTapped)
+                }
+                if let fact = store.fact {
+                    Text(fact)
+                }
+            }
+            Section {
+                if store.isTimerOn {
+                    Button("Stop timer") {
+                        store.send(.toggleTimerButtonTapped)
+                    }
+                } else {
+                    Button("Start timer") {
+                        store.send(.toggleTimerButtonTapped)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(
+        store: Store(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+    )
 }
+
--- Todo/Todo/TodoApp.swift
+++ Todo/Todo/TodoApp.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct TodoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(store: Store(initialState: CounterFeature.State()) {
+                CounterFeature()
+            })
         }
     }
 }
