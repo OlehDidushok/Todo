6395f4cfbd9112273d09aa7280fabee88e8ffdf9
Oleh Didushok <didushokov@gmail.com>
2025-09-24 19:28:07 +0300

    Initial commit

---
+++ Todo/Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Oleh on 24.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
